19e20aa74644a91e8594c613d0a30cec03d081e9
yuliao <spyfour30810@gmail.com>
2020-12-27 01:36:08 +0800

    crawler test

--- fsm.py
+++ fsm.py
@@ -4,24 +4,24 @@ from utils import send_text_message
 
 import requests
 
-def get_all_href(num, url):
-    dict ={}
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "html.parser")
-    results = soup.select("div.title")
-    for item in results:
-        a_item = item.select_one("a")
-        title = item.text
-        if a_item:
-            if '經驗' in title and num == 0:
-                dict[title] = 'https://www.ptt.cc'+ a_item.get('href')
-    return dict
+
 
 
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
-    
+    def get_all_href(num, url):
+        content = ""
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, "html.parser")
+        results = soup.select("div.title")
+        for item in results:
+            a_item = item.select_one("a")
+            title = item.text
+            if a_item:
+                if '經驗' in title and num == 0:
+                    content = title + "https://www.ptt.cc"+ a_item.get('href')
+        return content
     def is_going_to_newest_page(self, event):
         text = event.message.text
         return text == "新文章"
@@ -36,7 +36,7 @@ class TocMachine(GraphMachine):
     def on_enter_experience_page(self, event):
         #send_text_message(event.reply_token, "trigger experience")
         url = "https://www.ptt.cc/bbs/marvel/index.html"
-        dict.update(get_all_href(0, url))
+        content = get_all_href(0, url)
         for page in range(1,3):
             r = requests.get(url)
             soup = BeautifulSoup(r.text,"html.parser")
@@ -44,11 +44,8 @@ class TocMachine(GraphMachine):
             up_page_href = btn[3]['href']
             next_page_url = 'https://www.ptt.cc' + up_page_href
             url = next_page_url
-            dict.update(get_all_href(0, url = url))
-        text=""
-        for key, value in dict.items():
-            text += key + value
-        send_text_message(event.reply_token, text) 
+            content = get_all_href(0, url = url)
+        send_text_message(event.reply_token, content) 
 
     def is_going_to_favorite_page(self, event):
         text = event.message.text
