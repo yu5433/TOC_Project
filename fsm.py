6a0a415e5d82f401cc923396a39719861d2487ce
yuliao <spyfour30810@gmail.com>
2020-12-26 23:28:32 +0800

    [modified] debug

--- fsm.py
+++ fsm.py
@@ -13,16 +13,13 @@ class TocMachine(GraphMachine):
 
     def on_enter_newest_page(self, event):
         send_text_message(event.reply_token, "請輸入欲搜尋分類")
-        self.go_back()
-    
+
     def is_going_to_favorite_page(self, event):
         text = event.message.text
         return text == "精選文章"
 
     def on_enter_newest_page(self, event):
-        reply_token = event.reply_token
-        send_text_message(reply_token, "請輸入欲查看文章")
-        self.go_back()
+        send_text_message(event.reply_token, "請輸入欲查看文章")
     
 
 """
