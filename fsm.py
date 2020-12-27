47c376073ddb253e62330ccc6acba1f661dedba5
yuliao <spyfour30810@gmail.com>
2020-12-27 16:46:51 +0800

    [debug]

--- fsm.py
+++ fsm.py
@@ -90,7 +90,7 @@ class TocMachine(GraphMachine):
             url = next_page_url
             content += get_txt(0, url = url)
         send_text_message(event.reply_token, content) 
-
+        self.go_back()
     def is_going_to_creation_page(self, event):
         text = event.message.text
         return text == "創作"
@@ -107,7 +107,7 @@ class TocMachine(GraphMachine):
             url = next_page_url
             content += get_txt(1, url = url)
         send_text_message(event.reply_token, content) 
-    
+        self.go_back()
     def is_going_to_translation_page(self, event):
         text = event.message.text
         return text == "翻譯"
@@ -124,25 +124,28 @@ class TocMachine(GraphMachine):
             url = next_page_url
             content += get_txt(2, url = url)
         send_text_message(event.reply_token, content) 
-
+        self.go_back()
     def is_going_to_hottest_page(self, event):
         text = event.message.text
         return text == "熱門文章"
     def on_enter_hottest_page(self, event):
-        content = "以下為近日熱門文章"
+        content = "以下為近日熱門文章\n"
         url = "https://www.ptt.cc/bbs/marvel/index.html"
         content+=get_hot_href(url)
+        content+="為您返回主畫面^__^\n"
         send_text_message(event.reply_token, content)
-    
+        self.go_back()
     def is_going_to_classical_page(self, event):
         text = event.message.text
         return text == "經典文章"
     def on_enter_classical_page(self, event):
-        content = "以下為Marvel版精選文章"
+        content = "以下為Marvel版精選文章\n"
         content += "[翻譯] Nosleep - 前房客留下了生存指南（1）\nhttps://www.ptt.cc/bbs/marvel/M.1566137420.A.7D1.html \n"
         content += "[翻譯] 日本怪談：深夜電臺廣播01\n https://www.ptt.cc/bbs/marvel/M.1577017556.A.2F5.html \n"
         content += "[翻譯] Nosleep-屍體化妝師\n https://www.ptt.cc/bbs/marvel/M.1578220152.A.886.html \n"
         content += "[翻譯] Nosleep-太空女孩\n https://www.ptt.cc/bbs/marvel/M.1579713441.A.EED.html \n"
         content += "[翻譯] Nosleep-我在網上徵到惡魔室友(1)\n https://www.ptt.cc/bbs/marvel/M.1543501533.A.E93.html \n"
         content += "[轉錄] 真人真事-不眠山\n https://www.ptt.cc/bbs/marvel/M.1446446701.A.FF1.html \n"
+        content += "為您返回主畫面^__^\n"
         send_text_message(event.reply_token, content)        
+        self.go_back()
